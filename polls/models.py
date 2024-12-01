7f61cb893cd89e7916b119c38b1487a7f711e656
Hoyong Lee <igotoweb@gmail.com>
2024-12-01 01:16:46 +0000

    fix serialize function

--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,15 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-from django.core.serializers import serialize
 from django.core.serializers.json import DjangoJSONEncoder
+from django.forms.models import model_to_dict
+import json
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
-        return serialize("json", Question.objects.all(), cls=DjangoJSONEncoder)
+        return json.dumps(model_to_dict(self), cls=DjangoJSONEncoder, ensure_ascii=False)
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -17,4 +18,4 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-        return serialize("json", Choice.objects.all(), cls=DjangoJSONEncoder)
+        return json.dumps(model_to_dict(self), cls=DjangoJSONEncoder, ensure_ascii=False)
