96a7dea2c92193c14bbc7ef960a0ce11af868bc7
Hoyong Lee <igotoweb@gmail.com>
2024-12-02 03:03:11 +0100

    add unit tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
 from django.test import TestCase
 
+from .models import Question
+from django.utils import timezone
+
+
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_length(self):
+        self.assertIs(len("hello"), 6)
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+
+        self.assertIs(future_question.was_published_recently(), False)
