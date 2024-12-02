6342cf7827f209a39199e9aac7e3db42e5376cfa
Hoyong Lee <igotoweb@gmail.com>
2024-12-02 04:02:59 +0100

    seperate initial script

---
+++ runserver.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+date > current_time.txt
+export SERVER_PID=$(ps -ef | grep runserver | awk '{ print $2 }' | head -1)
+echo $SERVER_PID
+
+# (만일 해당 디렉토리가 있으면)
+if [ -d $HOME/django-tutorial ]; then
+  # git pull을 실행한다
+  cd django-tutorial
+  git pull origin main
+else # (없으면)
+  # git repository로부터 파일을 클론한다
+  git clone https://github.com/gotoweb/django-tutorial/
+  cd django-tutorial
+fi
+
+# 가상 환경 설정
+rm -rf .venv # 기존에 설치된 것 있으면 지우고
+python3 -m venv .venv
+source .venv/bin/activate
+
+# 종속성 설치
+pip3 install -r requirements.txt
+
+# 종속성이 뭐가 설치되었을까?
+pip3 freeze
+
+# 서버가 실행중이면 끄고
+echo $SERVER_PID
+kill -15 $SERVER_PID
+
+# 서버 새로 실행 (백그라운드로)
+nohup python3 manage.py runserver > server.log 2>&1 &
